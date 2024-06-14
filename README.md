9bec1d0a105434d64012a92f6197758bb8b4dd95
Yousef Abu Shanab <josephyousef249@gmail.com>
2024-06-14 21:31:34 +0300

    convert to package

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # drf-ai-ms
+
 Django REST Framework micro service for common AI tasks using HuggingFace Hub
---
+++ drf_ai_ms/__init__.py
@@ -0,0 +1,6 @@
+""" DRF AI Microservice """
+
+# Load .env file with HF_TOKEN
+from dotenv import load_dotenv
+
+load_dotenv()
